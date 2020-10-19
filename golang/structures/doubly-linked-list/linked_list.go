254cd894049ddd12e36523d930cbcae96642f3d5
Flávio Tonon <flavioltonon@gmail.com>
2020-10-19 17:52:17 -0300

    fix doubly linked list test

--- golang/structures/doubly-linked-list/linked_list.go
+++ golang/structures/doubly-linked-list/linked_list.go
@@ -3,7 +3,8 @@ package doubly
 // DoublyLinkedList represents a Linked List data structure, which should contain a series of
 // nodes pointing to the next node.
 type DoublyLinkedList struct {
-	head   *Node
+	first  *Node
+	last   *Node
 	length int
 }
 
@@ -21,7 +22,7 @@ func (l *DoublyLinkedList) Len() int {
 func (l *DoublyLinkedList) Values() []string {
 	values := make([]string, 0, l.length)
 
-	current := l.head
+	current := l.first
 
 	for current != nil {
 		values = append(values, current.value)
@@ -31,43 +32,84 @@ func (l *DoublyLinkedList) Values() []string {
 	return values
 }
 
-// Head returns a pointer to the first node in the DoublyLinkedList
-func (l *DoublyLinkedList) Head() *Node {
-	return l.head
+// First returns a pointer to the first node in the DoublyLinkedList
+func (l *DoublyLinkedList) First() *Node {
+	return l.first
+}
+
+// Last returns a pointer to the last node in the DoublyLinkedList
+func (l *DoublyLinkedList) Last() *Node {
+	return l.last
 }
 
 // Search returns true if the input value exist in any of its nodes and returns false otherwise in
 // an O(n) time complexity.
 func (l *DoublyLinkedList) Search(value string) bool {
-	return l.head.search(value)
+	return l.first.search(value)
 }
 
-// Add adds a new value to the beginning of the DoublyLinkedList with an O(1) time complexity
+// Prepend adds a new value to the beginning of the DoublyLinkedList with an O(1) time complexity
 func (l *DoublyLinkedList) Prepend(value string) {
 	new := &Node{
 		value: value,
 	}
 
-	if l.head != nil {
-		new.next = l.head
+	if l.first != nil {
+		new.next = l.first
 		new.next.previous = new
+	} else {
+		l.last = new
 	}
 
-	l.head = new
+	l.first = new
+	l.length++
+}
+
+// Append adds a new value to the end of the DoublyLinkedList with an O(1) time complexity
+func (l *DoublyLinkedList) Append(value string) {
+	new := &Node{
+		value: value,
+	}
+
+	if l.last != nil {
+		l.last.next = new
+		new.previous = l.last
+	} else {
+		l.first = new
+	}
+
+	l.last = new
 	l.length++
 }
 
 // Delete removes a value from the DoublyLinkedList (if it exists), minding the order of the nodes values
 func (l *DoublyLinkedList) Delete(value string) bool {
-	deleted := l.head.delete(value)
+	n := l.first
 
-	if !deleted {
-		return false
+	for n != nil {
+		if n.value != value {
+			n = n.next
+			continue
+		}
+
+		if n.next == nil {
+			l.last = n.previous
+		} else {
+			n.next.previous = n.previous
+		}
+
+		if n.previous == nil {
+			l.first = n.next
+		} else {
+			n.previous.next = n.next
+		}
+
+		l.length--
+
+		return true
 	}
 
-	l.length--
-
-	return true
+	return false
 }
 
 // Node represents a single node of Doubly Linked List data structures, which should contain
@@ -83,14 +125,14 @@ func (n *Node) Value() string {
 	return n.value
 }
 
-// Next returns a pointer to the node previous to the current node
-func (n *Node) Previous() *Node {
-	return n.previous
-}
-
 // Next returns a pointer to the node next to the current node
 func (n *Node) Next() *Node {
 	return n.next
+}
+
+// Previous returns a pointer to the node previous to the current node
+func (n *Node) Previous() *Node {
+	return n.previous
 }
 
 func (n *Node) search(value string) bool {
@@ -101,21 +143,6 @@ func (n *Node) search(value string) bool {
 	if n.value != value {
 		return n.next.search(value)
 	}
-
-	return true
-}
-
-func (n *Node) delete(value string) bool {
-	if n == nil {
-		return false
-	}
-
-	if n.value != value {
-		return n.next.delete(value)
-	}
-
-	n.next.previous = n.previous
-	n.previous.next = n.next
 
 	return true
 }
--- golang/structures/doubly-linked-list/linked_list_test.go
+++ golang/structures/doubly-linked-list/linked_list_test.go
@@ -10,24 +10,50 @@ import (
 
 func TestLinkedList(t *testing.T) {
 	input := []string{"7", "2", "1", "4", "9", "6", "0", "3", "8", "5"}
-	expected := []string{"5", "8", "3", "0", "6", "9", "4", "1", "2", "7"}
 
-	list := doubly.NewLinkedList()
+	t.Run("Prepend", func(t *testing.T) {
+		expected := []string{"5", "8", "3", "0", "6", "9", "4", "1", "2", "7"}
 
-	for _, v := range input {
-		list.Prepend(v)
-	}
+		list := doubly.NewLinkedList()
 
-	assert.Equal(t, len(input), list.Len())
+		for _, v := range input {
+			list.Prepend(v)
+		}
 
-	for i, value := range list.Values() {
-		assert.Equal(t, expected[i], value)
-	}
+		assert.Equal(t, len(input), list.Len())
 
-	assert.Equal(t, false, list.Search("10"))
-	assert.Equal(t, true, list.Search("1"))
-	assert.Equal(t, true, list.Search("5"))
+		for i, value := range list.Values() {
+			assert.Equal(t, expected[i], value)
+		}
 
-	assert.Equal(t, true, list.Delete("3"))
-	assert.Equal(t, false, list.Search("3"))
+		assert.Equal(t, false, list.Search("10"))
+		assert.Equal(t, true, list.Search("1"))
+		assert.Equal(t, true, list.Delete("5"))
+		assert.Equal(t, false, list.Search("5"))
+		assert.Equal(t, true, list.Delete("7"))
+		assert.Equal(t, false, list.Search("7"))
+	})
+
+	t.Run("Append", func(t *testing.T) {
+		expected := []string{"7", "2", "1", "4", "9", "6", "0", "3", "8", "5"}
+
+		list := doubly.NewLinkedList()
+
+		for _, v := range input {
+			list.Append(v)
+		}
+
+		assert.Equal(t, len(input), list.Len())
+
+		for i, value := range list.Values() {
+			assert.Equal(t, expected[i], value)
+		}
+
+		assert.Equal(t, false, list.Search("10"))
+		assert.Equal(t, true, list.Search("1"))
+		assert.Equal(t, true, list.Delete("5"))
+		assert.Equal(t, false, list.Search("5"))
+		assert.Equal(t, true, list.Delete("7"))
+		assert.Equal(t, false, list.Search("7"))
+	})
 }
